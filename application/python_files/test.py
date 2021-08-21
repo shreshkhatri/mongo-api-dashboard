b2aa1a6efead78174898a933aeff4f0dd11d3f43
Suresh Khatri <38423131+shreshkhatri@users.noreply.github.com>
2021-08-21 21:42:07 +0100

    Delete application directory

--- application/python_files/test.py
+++
@@ -1,8 +0,0 @@
-from datetime import date,datetime
-import time
-
-print(str(datetime.now().date()))
-
-print(int(time.mktime(datetime.strptime("2021-08-13", "%Y-%m-%d").timetuple())))
-
-print(time.mktime(datetime.strptime(str(date.today()), "%Y-%m-%d").timetuple())+86400)
